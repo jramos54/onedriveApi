67d0effd923a62b7ca0314077360580d58a51d9d
jramos54 <jrmsmolina54@gmail.com>
2024-02-15 12:50:19 -0600

    sql_server change

--- test/sql_server.py
+++ test/sql_server.py
@@ -1,8 +1,18 @@
 import pymssql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+USER=os.getenv("USER")
+PASSWORD=os.getenv("PASSWORD")
+DATABASE=os.getenv("DATABASE")
+HOST_DATABASE=os.getenv("HOST_DATABASE")
+DATABASE_DRIVE=os.getenv("DATABASE_DRIVE")
+PORT=os.getenv("PORT_DATABASE")
 
 try:
-    conn = pymssql.connect(server='CCAZR-ORIG01\\ORIG_PLATAFORMA', port='1433', user='RPAUsr.sql', password='H4y40M1y4z4k!24', database='MonitorISCAM')
-    
+    conn = pymssql.connect(server=HOST_DATABASE, port=PORT, user=USER, password=PASSWORD, database=DATABASE)
     print("Conexión exitosa")
     conn.close()
 except Exception as e:
