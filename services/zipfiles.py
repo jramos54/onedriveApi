31bbf2725b817abbc005a612e21217a12258b957
jramos54 <jrmsmolina54@gmail.com>
2024-02-16 10:23:52 -0600

    zipfiles

--- services/zipfiles.py
+++ services/zipfiles.py
@@ -1,19 +1,23 @@
 import os
 import zipfile
-import shutil
-import asyncio
 from pathlib import Path
+import shutil  
 
 class ZipFiles:
-    def __init__(self,file_name:str):
-        self.archivo_zip=f"{file_name}.zip"
+    def __init__(self, file_name: str):
+        self.archivo_zip = f"{file_name}.zip"
         
-    async def comprimir_directorio(self,directorio: str) -> str:
+    async def comprimir_directorio(self, directorio: str) -> str:
         with zipfile.ZipFile(self.archivo_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for root, dirs, files in os.walk(directorio):
                 for file in files:
                     zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(directorio, '..')))
-        return os.path.abspath(self.archivo_zip)
+        
+        ruta_final_zip = os.path.join(directorio, self.archivo_zip)
+        shutil.move(self.archivo_zip, ruta_final_zip)
+
+        return os.path.abspath(ruta_final_zip)
 
     async def eliminar_archivo_zip(self):
         Path(self.archivo_zip).unlink(missing_ok=True)
+
--- test/zip.py
+++ test/zip.py
@@ -5,8 +5,9 @@ import asyncio
 from services.zipfiles import ZipFiles
 
 async def main():
-    zipObject = ZipFiles('test')
-    dir_resultante=await zipObject.comprimir_directorio("test_dir")
+    directory="C:\\Users\\jramos\\codingFiles\\dacodes\\scrapping_project_iscam\\GS1\\excel_files"
+    zipObject = ZipFiles('zip_comprension_2')
+    dir_resultante=await zipObject.comprimir_directorio(directory)
     print(dir_resultante)
     
 if __name__ == "__main__":
